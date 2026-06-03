23db93992891ffe22be9abed1726b79a2ec8a5ce
Matt Hughes <matth@eidetic.net.au>
2026-06-03 12:37:31 +1000

    align serialize/deserialize with pointcaster qt branch 2026

--- include/pointclouds.h
+++ include/pointclouds.h
@@ -1,20 +1,21 @@
 #pragma once
 
-#include <cstdint>
 #include <cstddef>
-#include <vector>
+#include <cstdint>
 #include <limits>
+#include <span>
+#include <vector>
 
 namespace bob::types {
 
- struct uint2 {
-   unsigned int x, y = 0;
+struct uint2 {
+  unsigned int x, y = 0;
 
-   bool operator==(const uint2 other) const {
-     return x == other.x && y == other.y;
-   }
-   bool operator!=(const uint2 other) const { return !operator==(other); }
- };
+  bool operator==(const uint2 other) const {
+    return x == other.x && y == other.y;
+  }
+  bool operator!=(const uint2 other) const { return !operator==(other); }
+};
 
 struct alignas(4) position {
   int16_t x = 0;
@@ -25,7 +26,7 @@ struct alignas(4) position {
 
 constexpr bool operator==(const position &lhs, const position &rhs) {
   return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z &&
-	 lhs.__pad == rhs.__pad;
+         lhs.__pad == rhs.__pad;
 }
 constexpr bool operator!=(const position &lhs, const position &rhs) {
   return !(rhs == lhs);
@@ -40,34 +41,44 @@ struct color {
   bool operator!=(const color other) const { return !operator==(other); }
 };
 
-  using bytes = std::vector<std::byte>;
+struct position_bounds {
+  position min{(std::numeric_limits<int16_t>::max)(),
+               (std::numeric_limits<int16_t>::max)(),
+               (std::numeric_limits<int16_t>::max)(), 0};
+  position max{(std::numeric_limits<int16_t>::min)(),
+               (std::numeric_limits<int16_t>::min)(),
+               (std::numeric_limits<int16_t>::min)(), 0};
+};
 
-  class PointCloud {
-  public:
-    std::vector<position> positions;
-    std::vector<color> colors;
+using bytes = std::vector<std::byte>;
 
-    auto size() const { return positions.size(); }
-    auto empty() const { return positions.empty(); }
+class PointCloud {
+public:
+  std::vector<position> positions;
+  std::vector<color> colors;
+  position_bounds bounds;
 
-    bytes serialize(bool compress = false) const;
-    static PointCloud deserialize(const bytes &buffer);
+  auto size() const { return positions.size(); }
+  auto empty() const { return positions.empty(); }
 
-  private:
-    bytes compress() const;
-    static PointCloud decompress(const bytes& buffer, unsigned long point_count);
-  };
+  bytes serialize(bool compress = false) const;
+  static PointCloud deserialize(std::span<const std::byte> buffer);
 
-  PointCloud operator+(PointCloud const& lhs, PointCloud const& rhs);
-  PointCloud operator+=(PointCloud& lhs, const PointCloud& rhs);
+private:
+  bytes compress() const;
+  static PointCloud decompress(const bytes &buffer, unsigned long point_count);
+};
 
-  struct PointCloudPacket {
-    // out packet needs these explicitly sized types to ensure portability
-    // between unix and windows systems
-    uint64_t timestamp;
-    uint64_t point_count;
-    uint8_t compressed;
-    bytes data;
-  };
+PointCloud operator+(PointCloud const &lhs, PointCloud const &rhs);
+PointCloud operator+=(PointCloud &lhs, const PointCloud &rhs);
 
-}
+struct PointCloudPacket {
+  // out packet needs these explicitly sized types to ensure portability
+  // between unix and windows systems
+  uint64_t timestamp;
+  uint64_t point_count;
+  uint8_t compressed;
+  bytes data;
+};
+
+} // namespace bob::types
